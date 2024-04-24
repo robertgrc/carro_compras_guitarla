709a1caa4e734b72bfd8945172149948ddb8c84a
robertgrc <robert.rodriguez.rgrc@gmail.com>
2024-04-24 12:00:43 -0400

    Detectando si el carrito tiene algun item o esta vacio

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -20,8 +20,10 @@ export default function Header({cart}){
                         <img className="img-fluid" src="./public/img/carrito.png" alt="imagen carrito" />
 
                         <div id="carrito" className="bg-white p-3">
-                            <p className="text-center">El carrito esta vacio</p>
-                            <table className="w-100 table">
+                            {cart.length===0? (
+                                <p className="text-center">El carrito esta vacio</p>
+                            ):(
+                                <table className="w-100 table">
                                 <thead>
                                     <tr>
                                         <th>Imagen</th>
@@ -68,6 +70,8 @@ export default function Header({cart}){
                                     ))}
                                 </tbody>
                             </table>
+                            )}
+                            
 
                             <p className="text-end">Total pagar: <span className="fw-bold">$899</span></p>
                             <button className="btn btn-dark w-100 mt-3 p-2">Vaciar Carrito</button>
